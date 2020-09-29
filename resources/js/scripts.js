759fbe3e6bec492ef596f9fdff0f0b71a56f0048
Ajani_Levy <jeremyconno7@gmail.com>
2020-09-28 20:13:12 -0400

    connected cart to footer

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -24,7 +24,9 @@ for (let i=0; i<data.length; ++i) {
     newDiv.appendChild(button)
     itemsContainer.appendChild(newDiv)
     }
-
+const itemList = document.getElementById('item-list')
+const cartQty = document.getElementById('cart-qty')
+const cartTotal = document.getElementById('cart-total')
 const cart = []
 const obj = {}
 function addItem(name, price){
@@ -40,39 +42,47 @@ function addItem(name, price){
 let alltotal = 0
 let allitems = 0
 function getItems(){
+    let qtyStr = ''
     for (let i = 0; i < cart.length; i += 1){
         allitems += cart[i].qty
     }
+    qtyStr = `${allitems}`
+    cartQty.innerHTML = qtyStr
 }
+itemList.innerHTML = '<li> Hello World </li>'
 function showItems(){
-    console.log(`You have ${allitems} items in your cart.`)
-    for (let i = 0; i < cart.length; i += 1) {
-        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    cartQty.innerHTML = (`You have ${allitems} items in your cart.`)
+    let itemStr = ''
+    const name = cart[i].name
+    const price = cart[i].price
+    const qty = cart[i].qty
 
+    for (let i = 0; i < cart.length; i += 1) {
+        console.log(`- ${name} $${price} x ${qty} = ${qty * price}`)
+        itemStr += `<li>${name} $${price} x ${qty}</li>`
     }
-    console.log(`Total in cart: $${getTotal()}`)
+    itemList.innerHTML = itemStr
+    cartTotal.innerHTML = `$${getTotal()}`
+
 }
 function getTotal(){
     for (let i = 0; i < cart.length; i += 1){
         alltotal += cart[i].price * cart[i].qty
     }
     return total.toFixed(2)}
-function removeItem(name){
-    for(let i = 0; i < cart.length; i += 1) {
-        if(cart[i].name === name){
-            cart.splice(i, 1)
-            return
-        }
-    }
-}
-function removesingleItem(name){
-    for(let i = 0; i < cart.length; i += 1) {
-        if(cart[i].name === name){
-            cart[i].qty -= 1
-            if (cart[i].qty === 0){
+function removeItem(name, qty = 0){
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name){
+            if (qty > 0) {
+                cart[i].qty -= qty
+            }
+            if (cart[i].qty  < 1 || qty === 0){
                 cart.splice(i, 1)
             }
             return
         }
-    }
 }
+}
+print(itemList)
+print(cartQty)
+print(cartTotal)
